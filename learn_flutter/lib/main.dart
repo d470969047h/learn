7b542f0d1df11d8543421daea4488f7aac17ad09
d470969047h <d470969047h@gmail.com>
2019-02-20 16:47:46 +0800

    widget的Container组件使用1

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -17,9 +17,25 @@ class MyApp extends StatelessWidget{
           title:Text('这里是测试标题1')
         ),// AppBar
         body:Center(
-
+          child: Container(
+            child: new Text('Hello DaiHui',style: TextStyle(fontSize: 40.0)),
+            alignment:Alignment.center, // 子容器对齐方式
+            width: 500.0,
+            height: 400.0,
+            color: Colors.lightBlue,
+          ),
         ),// Center
       ),// Scaffold
     );// MaterialApp
   }
 }
+
+//          bottomCenter:下部居中对齐。
+//          botomLeft: 下部左对齐。
+//          bottomRight：下部右对齐。
+//          center：纵横双向居中对齐。
+//          centerLeft：纵向居中横向居左对齐。
+//          centerRight：纵向居中横向居右对齐。
+//          topLeft：顶部左侧对齐。
+//          topCenter：顶部居中对齐。
+//          topRight： 顶部居左对齐。
