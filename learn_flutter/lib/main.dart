bb765e1901f6f3909626b68b23842c8e1517b606
d470969047h <d470969047h@gmail.com>
2019-02-21 09:45:03 +0800

    widget的Container组件使用2

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -22,7 +22,16 @@ class MyApp extends StatelessWidget{
             alignment:Alignment.center, // 子容器对齐方式
             width: 500.0,
             height: 400.0,
-            color: Colors.lightBlue,
+//            color: Colors.lightBlue,
+            padding:const EdgeInsets.fromLTRB(10.0,30.0,0.0,0.0),// 设置Container的上边距为30，左边距为10
+            margin: const EdgeInsets.all(20.0), // 外边距
+            /// decoration是 container 的修饰器，主要的功能是设置背景和边框。
+            /// 比如你需要给背景加入一个渐变，这时候需要使用BoxDecoration这个类，
+            /// 代码如下（需要注意的是如果你设置了decoration，就不要再设置color属性了，因为这样会冲突）
+            decoration: new BoxDecoration(
+              gradient: const LinearGradient(colors: [Colors.lightBlue,Colors.lightGreen,Colors.amberAccent]),
+              border: Border.all(width: 20.0,color: Colors.red)
+            ),
           ),
         ),// Center
       ),// Scaffold
