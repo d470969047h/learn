0be0a776a245294bd2d1989e8601298d7df2e60f
d470969047h <d470969047h@gmail.com>
2019-02-22 11:24:00 +0800

    组合模式

---
+++ learn-design-pattern/src/main/java/com/daihui/composite/code/setp1/Employee.java
@@ -0,0 +1,44 @@
+package com.daihui.composite.code.setp1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: daihui
+ * @Description: 步骤 1 创建 Employee 类，该类带有 Employee 对象的列表。
+ * @Date: Created in 11:16 2019-02-22
+ * @Modified By:
+ */
+public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return ("Employee :[ Name : " + name
+                + ", dept : " + dept + ", salary :"
+                + salary + " ]");
+    }
+}
---
+++ learn-design-pattern/src/main/java/com/daihui/composite/code/setp2/CompositePatternDemo.java
@@ -0,0 +1,44 @@
+package com.daihui.composite.code.setp2;
+
+import com.daihui.composite.code.setp1.Employee;
+
+/**
+ * @Description: 步骤 2 使用 Employee 类来创建和打印员工的层次结构。
+ * @Date: Created in 11:21 2019-02-22
+ * @Author: daihui
+ * @Modified By:
+ */
+public class CompositePatternDemo {
+
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John", "CEO", 30000);
+
+        Employee headSales = new Employee("Robert", "Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel", "Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura", "Marketing", 10000);
+        Employee clerk2 = new Employee("Bob", "Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard", "Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //打印该组织的所有员工
+        System.out.println(CEO);
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
