4b7fd0e1b7a8bdd4645dd6d2938b1c97525cd9bd
d470969047h <d470969047h@gmail.com>
2019-04-02 19:59:39 +0800

    ignore file

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -1,50 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(title: '页面跳转返回数据', home: FirstPage()));
+}
 
-
-
-class MyApp extends StatelessWidget{
-
+/// 首页
+class FirstPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('找小姐姐要电话')),
+      body: Center(
+        child: RouteButton(),
+      ),
+    );
+  }
+}
+class RouteButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        _navigateToXiaoJieJie(context);
+      },
+      child: Text('去找小姐姐'),
+    );
+  }
 
-    return MaterialApp(
-      title:'daihui123',
+  _navigateToXiaoJieJie(BuildContext context) async {
+    // async是启用异步方法
 
-      home:Scaffold(
-        appBar:AppBar(
-          title:Text('这里是测试标题1')
-        ),// AppBar
-        body:Center(
-          child: Container(
-            child: new Text('Hello DaiHui',style: TextStyle(fontSize: 40.0)),
-            alignment:Alignment.center, // 子容器对齐方式
-            width: 500.0,
-            height: 400.0,
-//            color: Colors.lightBlue,
-            padding:const EdgeInsets.fromLTRB(10.0,30.0,0.0,0.0),// 设置Container的上边距为30，左边距为10
-            margin: const EdgeInsets.all(20.0), // 外边距
-            /// decoration是 container 的修饰器，主要的功能是设置背景和边框。
-            /// 比如你需要给背景加入一个渐变，这时候需要使用BoxDecoration这个类，
-            /// 代码如下（需要注意的是如果你设置了decoration，就不要再设置color属性了，因为这样会冲突）
-            decoration: new BoxDecoration(
-              gradient: const LinearGradient(colors: [Colors.lightBlue,Colors.lightGreen,Colors.amberAccent]),
-              border: Border.all(width: 20.0,color: Colors.red)
-            ),
-          ),
-        ),// Center
-      ),// Scaffold
-    );// MaterialApp
+    final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => XiaoJieJie()));
+
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text('${result}')));
   }
 }
 
-//          bottomCenter:下部居中对齐。
-//          botomLeft: 下部左对齐。
-//          bottomRight：下部右对齐。
-//          center：纵横双向居中对齐。
-//          centerLeft：纵向居中横向居左对齐。
-//          centerRight：纵向居中横向居右对齐。
-//          topLeft：顶部左侧对齐。
-//          topCenter：顶部居中对齐。
-//          topRight： 顶部居左对齐。
+class XiaoJieJie extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('我是小姐姐')),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+                child: Text('大长腿小姐姐'),
+                onPressed: () {
+                  Navigator.pop(context, '大长腿：135123412234');
+                }),
+            RaisedButton(
+                child: Text('小蛮腰小姐姐'),
+                onPressed: () {
+                  Navigator.pop(context, '小蛮腰：135123412234');
+                }),
+            RaisedButton(
+                child: Text('大粗腿小姐姐'),
+                onPressed: () {
+                  Navigator.pop(context, '大粗腿：135123412234');
+                }),
+            RaisedButton(
+                child: Text('45岁的小姐姐'),
+                onPressed: () {
+                  Navigator.pop(context, '45岁：135123412234');
+                }),
+            RaisedButton(
+                child: Text('人妖小姐姐'),
+                onPressed: () {
+                  Navigator.pop(context, '人妖：135123412234');
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
